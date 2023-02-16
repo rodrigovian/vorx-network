08ba1e071055a451ce93423687d605efe809b0af
root <root@ip-172-31-27-35.ec2.internal>
2023-02-16 00:04:05 +0000

    Add: Creating Subnets

--- networks.tf
+++ networks.tf
@@ -6,6 +6,28 @@ resource "aws_vpc" "vorx-vpc-prod" {
   }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1a" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public-Subnet-1a"
+  }
+}
+
+
+resource "aws_subnet" "vorx-subnet-priv-1a" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private-Subnet-1a"
+  }
+}
+
+
 
 ## OUTPUTS ##
 output "vpc_vorx_prod_id" {
@@ -15,4 +37,14 @@ output "vpc_vorx_prod_id" {
 output "vpc_vorx_prod_arn" {
   value = aws_vpc.vorx-vpc-prod.arn
 }
+
+output "vorx_prod_subnet_pub" {
+  value = aws_subnet.vorx-subnet-pub-1a.id
+}
+
+output "vorx_prod_subnet_priv" {
+  value = aws_subnet.vorx-subnet-priv-1a.id
+}
+
+
 
