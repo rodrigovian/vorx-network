596f144df8943e64f124e13860d5209ff5ba5746
terraform-server <terraform-server@ec2.internal>
2023-02-16 01:27:37 +0000

    Add: Remote Backend Config

--- networks.tf
+++ networks.tf
@@ -84,6 +84,32 @@ resource "aws_route_table_association" "pub-rt-1b-associate" {
 
 
 
+resource "aws_route_table" "private-rt-1a" {
+  vpc_id = aws_vpc.vorx-vpc-prod.id
+
+  tags = {
+    Name = "prod-private-rt-1a"
+  }
+}
+
+resource "aws_route_table_association" "priv-rt-1a-associate" {
+  subnet_id      = aws_subnet.vorx-subnet-priv-1a.id
+  route_table_id = aws_route_table.private-rt-1a.id
+}
+
+
+resource "aws_route_table" "private-rt-1b" {
+  vpc_id = aws_vpc.vorx-vpc-prod.id
+
+  tags = {
+    Name = "prod-private-rt-1b"
+  }
+}
+
+resource "aws_route_table_association" "priv-rt-1b-associate" {
+  subnet_id      = aws_subnet.vorx-subnet-priv-1b.id
+  route_table_id = aws_route_table.private-rt-1b.id
+}
 
 ## OUTPUTS ##
 output "vpc_vorx_prod_id" {
