df261a167ee452cc3cd8f723dbcc16b6ca471b94
terraform-server <terraform-server@ec2.internal>
2023-02-16 00:42:59 +0000

    Add: Creating IGW

--- networks.tf
+++ networks.tf
@@ -50,6 +50,15 @@ resource "aws_subnet" "vorx-subnet-priv-1b" {
 }
 
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vorx-vpc-prod.id
+
+  tags = {
+    Name = "igw-prod-vorx-vpc"
+  }
+}
+
+
 ## OUTPUTS ##
 output "vpc_vorx_prod_id" {
   value = aws_vpc.vorx-vpc-prod.id
